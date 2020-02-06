79ca19725c53989d7f68c496fb291e9c32c0ca96
Will Prendergast <prendygast@hotmail.com>
2020-02-06 22:04:27 +0000

    Moving onto user profiles

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
-from accounts.forms import UserLoginForm
+from accounts.forms import UserLoginForm, UserRegistrationFrom
 # Create your views here.
 
 
@@ -36,3 +36,29 @@ def login(request):
     else:
         login_form = UserLoginForm()
     return render(request, 'login.html', {"login_form": login_form})
+
+
+def registration(request):
+    """ render the registration page """
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+
+    if request.method == "POST":
+        registration_form = UserRegistrationFrom(request.POST)
+
+        if registration_form.is_valid():
+            registration_form.save()
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password1'])
+
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully registered")
+                return redirect(reverse('index'))
+            else:
+                messages.error(request, "Unable to register your account at this time")
+
+    else:
+        registration_form = UserRegistrationFrom()
+    return render(request, 'registration.html',
+                  {'registration_form': registration_form})
